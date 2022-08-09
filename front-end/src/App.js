dbb259fffea51b44ee4e02c9e7dc9863e5b028a8
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-09 16:50:51 +0300

    implemented react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePageScreen from './screens/HomePageScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-<>
+<Router>
 <Header/>
 <main className='py-3'>
 <Container>
-<HomePageScreen/>
+  <Routes>
+<Route exact path='/'   element={<HomePageScreen/>}/>
+<Route  path='/product/:id' element={<ProductScreen/>}/>
+</Routes>
 </Container>
 </main>
 <Footer/>
-</>
-  );
+</Router>
+  )
 }
 
 export default App;
