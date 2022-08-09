6945a2ea790d5e6439e0fe964f0118f5f4054480
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-09 10:36:29 +0300

    created a home screen and displayed products froma json file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import HomePageScreen from './screens/HomePageScreen';
 function App() {
   return (
 <>
 <Header/>
 <main className='py-3'>
 <Container>
-<h1>Welcome to Proshop</h1>
+<HomePageScreen/>
 </Container>
 </main>
 <Footer/>
