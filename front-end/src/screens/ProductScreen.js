082b0a3b2c0559240485215cb0ac7fe43b221a04
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-10 10:46:37 +0300

    create a product screen page

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import {Link, useParams} from 'react-router-dom'
+import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
+import Ratings from '../components/Ratings'
+import products  from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({match}) => {
+    const {id}=useParams();
+    const product=products.find((p)=> p._id ===id)
   return (
-    <div>ProductScreen</div>
+    <>
+    <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+    <Row>
+        <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <Ratings value={product.numReviews} text={`${product.numReviews} reviews`}/>
+                </ListGroup.Item>
+                <ListGroup.Item>price:${product.price}</ListGroup.Item>
+                <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+            </ListGroup>
+        </Col>
+
+        <Col md={3}>
+            <Card>
+                <ListGroup variant='flush'>
+                <ListGroup.Item>
+        <Row>
+            <Col>price:</Col>
+            <Col><strong>${product.price}</strong></Col>
+        </Row> 
+    </ListGroup.Item>
+    <ListGroup.Item>
+    <Row>
+            <Col>Status:</Col>
+            <Col>
+            {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+            </Col>
+        </Row> 
+    </ListGroup.Item>
+    <ListGroup.Item> 
+        <Button variant="primary" disabled={product.countInStock === 0}>
+        Add To Cart         
+        </Button>
+    </ListGroup.Item>
+    </ListGroup>
+            </Card>
+        </Col>
+    </Row>
+ 
+    </>
   )
 }
 
