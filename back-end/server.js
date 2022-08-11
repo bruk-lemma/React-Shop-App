86ca4f022fe4200f1607b957f592346bc21be587
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-11 11:33:25 +0300

    adding colors to the respective console messages

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
+import colors from'colors'
 
 
 dotenv.config()
@@ -23,4 +24,4 @@ app.get('/api/products/:id',(req,res)=>{
 })
 
 const PORT=process.env.PORT || 5000
-app.listen(PORT,console.log(`backend running in ${process.env.NODE_ENV} mode on port ${ PORT}`))
+app.listen(PORT,console.log(`backend running in ${process.env.NODE_ENV} mode on port ${ PORT}`.yellow.bold))
