70e19b9335089812943ef4c0445d585b0e621602
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-15 12:19:56 +0300

    created product detail reducer and actions

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -1,77 +1,91 @@
-import React,{useEffect,useState} from 'react'
-import {Link, useParams} from 'react-router-dom'
-import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
-import Ratings from '../components/Ratings'
-import axios from 'axios' 
+import React, {useEffect} from "react";
+import {Link, useParams} from "react-router-dom";
+import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import Ratings from "../components/Ratings";
+import {listProductDetails} from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+//import axios from 'axios'
 //import products  from '../products'
 
-
-
 const ProductScreen = ({match}) => {
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const {loading, error, product} = productDetails;
 
-const [product,setProducts]=useState({})
-const {id}=useParams();
+  const {id} = useParams();
 
-useEffect(()=>{
-    const fetchProduct=async ()=>{
-        const {data}=await axios.get(`/api/products/${id}`)
-    
-        setProducts(data)
-    }
-    fetchProduct()
-},[id]) 
+  useEffect(() => {
+    dispatch(listProductDetails(id));
+  }, [dispatch, id]);
 
-    //const {id}=useParams();
-    //const product=products.find((p)=> p._id ===id)
+  //const {id}=useParams();
+  //const product=products.find((p)=> p._id ===id)
   return (
     <>
-    <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-    <Row>
-        <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid/>
-        </Col>
-        <Col md={3}>
-            <ListGroup variant='flush'>
-                <ListGroup.Item>
-                    <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Ratings value={product.numReviews} text={`${product.numReviews} reviews`}/>
-                </ListGroup.Item>
-                <ListGroup.Item>price:${product.price}</ListGroup.Item>
-                <ListGroup.Item>Description:{product.description}</ListGroup.Item>
-            </ListGroup>
-        </Col>
-
-        <Col md={3}>
-            <Card>
-                <ListGroup variant='flush'>
-                <ListGroup.Item>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <Row>
-            <Col>price:</Col>
-            <Col><strong>${product.price}</strong></Col>
-        </Row> 
-    </ListGroup.Item>
-    <ListGroup.Item>
-    <Row>
-            <Col>Status:</Col>
-            <Col>
-            {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-            </Col>
-        </Row> 
-    </ListGroup.Item>
-    <ListGroup.Item> 
-        <Button variant="primary" disabled={product.countInStock === 0} >
-        Add To Cart         
-        </Button>
-    </ListGroup.Item>
-    </ListGroup>
-            </Card>
-        </Col>
-    </Row>
- 
-    </>
-  )
-}
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Ratings
+                  value={product.numReviews}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>price:${product.price}</ListGroup.Item>
+              <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+            </ListGroup>
+          </Col>
 
-export default ProductScreen
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button
+                    variant='primary'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,15 +1,22 @@
-import {createStore,combineReducers,applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers'
-const reducer=combineReducers({
-      productList:productListReducer
-})
-console.log(reducer)
-const initialState={}
-const middleware=[thunk]
+import {createStore, combineReducers, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
+console.log(reducer);
+const initialState = {};
+const middleware = [thunk];
 
-const store=createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
-
+export default store;
