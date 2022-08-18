4f2d6715e491ed4654694ede61e1790c193e6300
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-18 10:03:24 +0300

    created a product controller

--- back-end/routes/productroutes.js
+++ back-end/routes/productroutes.js
@@ -1,31 +1,10 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {getProducts, getProduct} from "../controllers/productController.js";
 
 const app = express();
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    //throw new Error("something went wrong...");
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProduct);
 
 export default router;
