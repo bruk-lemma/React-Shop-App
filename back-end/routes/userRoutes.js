85996b61dc5c6e88daa9e8c936a9ebde25cf1628
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-24 11:33:27 +0300

    create a user registration end point and encrypted athe password

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import {authUser, getUserProfile} from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  RegisterUser,
+} from "../controllers/userController.js";
 import {protect} from "../middleware/authMiddleware.js";
 const app = express();
 const router = express.Router();
 
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile);
+router.route("/register").post(RegisterUser);
+
 //router.route("/:id").get(getProduct);
 
 export default router;
