410ad4ac0f5dc4851694f7f4a6553eb7e2366e76
Bruk Lemma <bruklemma2017@gmail.com>
2022-08-26 11:47:20 +0300

    created a user profile update endpoint

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -3,13 +3,17 @@ import {
   authUser,
   getUserProfile,
   RegisterUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import {protect} from "../middleware/authMiddleware.js";
 const app = express();
 const router = express.Router();
 
 router.route("/login").post(authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route("/register").post(RegisterUser);
 
 //router.route("/:id").get(getProduct);
